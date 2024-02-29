5a6f792fc1e3273f75956c4eb026fd414b419cdd
discoverinfonet <discoverinfonet@gmail.com>
2024-02-29 22:37:42 +0500

    added new files

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
 console.log('Hello World');
 console.log('This is session 5');
 console.log('this is git init');
+
+// git clone
+// git init
+// git add main.ts
+// git commit -m "this is my first"
+// git branch     //main  //master
+// git push origin master  
